b5a6806339d041e4460b161e334658db8aad3f6e
WingChhun <chhunjamesdc2@yahoo.com>
2018-05-25 12:47:52 -0700

    Add CardList, add async await way of contacting API using props and updating state

--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -1,24 +1,44 @@
 import React, {Component} from 'react';
 
-
+import "../../main.css"; //import css
 //import card component
 import Card from "./Card";
 
+const API_URL = "https://rickandmortyapi.com/api/character/";
 class CardList extends Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.updateAPI = this.props.updateAPI;
 
+    }
+    //handle API
+    async componentDidMount()
+    {
+
+        const res = await fetch(API_URL);
+        const {info, results} = await res.json();
+
+        //set state, using function passed from main container
+        this
+            .props
+            .updateAPI(info, results);
+        console.log("this.props.accessAPI", this.props.accessAPI);
+    }
     render()
     {
+
         return (
-            <div>
-            {/*Card*/}
+            <div className="cardlist">
                 <h1>CardList</h1>
+                <Card/>
+
             </div>
 
         );
     }
 
 }
-
 
 export default CardList;
