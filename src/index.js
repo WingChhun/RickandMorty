66309fdea374739b22af19b636b963b9a88a6e98
WingChhun <capt.wing.chhun@gmail.com>
2018-05-25 16:12:51 -0700

    Install dependencies, include main.css in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import "index.css";
-
+import "./main.css";
 import registerServiceWorker from './registerServiceWorker';
 
 //import components
 import Router from "./containers/Router";
 
-
 //render
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(
+    <Router/>, document.getElementById('root'));
 registerServiceWorker();
