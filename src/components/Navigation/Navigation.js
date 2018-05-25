fe7736ea2146e3a17b416af8ec71dc9c6ef39755
WingChhun <capt.wing.chhun@gmail.com>
2018-05-24 21:28:09 -0700

    Add Footer and navigation js files

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Navigation extends Component
+{
+
+    render()
+    {
+        return (
+            <div>
+                <h1>Navigation</h1>
+            </div>
+
+        );
+    }
+
+}
+
+export default Navigation;
