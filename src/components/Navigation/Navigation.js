c1c151f68cf8cd741f8fd08063666093a87845da
WingChhun <chhunjamesdc2@yahoo.com>
2018-05-25 12:48:04 -0700

    No rela changes, starter navigation layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 
+//import css
+import "../../main.css";
+
 class Navigation extends Component
 {
 
     render()
     {
         return (
-            <div>
-                <h1>Navigation</h1>
+            <div className="nav">
+                <h1>Rick and Morty Character List</h1>
+
             </div>
 
         );
