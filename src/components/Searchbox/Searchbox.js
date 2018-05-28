1492a55e6daaa251ae8a0a0936ad703adb499a66
WingChhun <capt.wing.chhun@gmail.com>
2018-05-27 23:51:42 -0700

    Added under sub text

--- src/components/Searchbox/Searchbox.js
+++ src/components/Searchbox/Searchbox.js
@@ -13,7 +13,12 @@ class Searchbox extends Component
                 <input
                     type="text"
                     onChange={updateSearch}
-                    placeholder="Enter a name to filter for on this page"/>
+                    placeholder="Enter a name to filter for on this page"/> {/*Font*/}
+                <p className="searchbox__under">This app is meant to be displayed on desktop, and was built using the
+                    <a href="rickandmortyapi.com"> Rick and Morty API
+                    </a>
+                </p>
+
             </div>
 
         );
