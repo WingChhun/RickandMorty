c440a24ca03511e946427b40373bc1697c68a7a0
WingChhun <capt.wing.chhun@gmail.com>
2018-05-27 20:24:20 -0700

    Finished CardList.js functionality, still need to add some markup and css to go along with the Card Component

--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-
+import PropTypes from 'prop-types';
 import "../../main.css"; //import css
 //import card component
 import Card from "./Card";
 
 const API_URL = "https://rickandmortyapi.com/api/character/";
+let API_INFO = {};
+let API_RESULTS = [];
 class CardList extends Component
 {
     constructor(props)
@@ -24,21 +26,45 @@ class CardList extends Component
         this
             .props
             .updateAPI(info, results);
+
+        //SEt API_INFO and API_RESULTS
+        // - Will be used for Card mapping
+        API_INFO = info;
+        API_RESULTS = results;
+        //DEBUG
         console.log("this.props.accessAPI", this.props.accessAPI);
+        console.log("API_INFO", API_INFO);
+        console.log("API_RESULTS", API_RESULTS);
     }
+    // @function mapCards @Purpose - Return Cards as List Items using
+    // this.state.results
     render()
     {
+        const {isConnected, resultsAPI} = this.props;
 
         return (
-            <div className="cardlist">
+            <div className="cardlist u--border-primary">
+                {this.mapCards}
                 <h1>CardList</h1>
-                <Card/>
 
+                {/*Display error message if not connected to API*/}
+                {(!this.props.isConnected && <h1>Could not connect to the API!</h1>)}
+
+                {/*Else render the cards*/}
+                {resultsAPI.map((card, index) => <Card key ={card.id} Card={card}>CardItem</Card>)}
             </div>
 
         );
     }
-
 }
-
+/* Proptypes check - Check info to receive an
+            object - is connected to receive a boolean - updateAPI to receive a function */
+CardList.propTypes = { //access this.state.info
+    infoAPI: PropTypes
+        .shape({count: PropTypes.number.isRequired})
+        .isRequired,
+    isConnected: PropTypes.bool.isRequired,
+    updateAPI: PropTypes.func.isRequired,
+    resultsAPI: PropTypes.array.isRequired
+}
 export default CardList;
