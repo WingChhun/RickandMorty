f1c3fe52bdef04c06b936e0a690de4144168417e
WingChhun <capt.wing.chhun@gmail.com>
2018-05-27 20:23:00 -0700

    Started markup for Card, added proptypes, converted to functional stateless component, and display the name of the card received from the API if there is any, if not have appropriate error message

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,18 +1,32 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-class Card extends Component
-{
+// functional stateless component set Card to null by default if nothing is
+// passed
+const Card = ({
+    Card = null
+}) => {
 
-    render()
-    {
-        return (
-            <div>
-                <h1>Card</h1>
-            </div>
+    return (
+        <div className="card">
+            {/*Error message*/}
+            {(Card === null && <h1>
+                No Data</h1>)}
 
-        );
-    }
+            {/*ELSE - Display card*/}
+            <h1>{Card.name}</h1>
+        </div>
+
+    );
 
 }
+//proptypes check
+Card.propTypes = {
 
+    Card: PropTypes
+        .shape({created: PropTypes.string.isRequired, gender: PropTypes.string.isRequired, id: PropTypes.number.isRequired, name: PropTypes.string.isRequired})
+        .isRequired
+
+};
+//propTypes export Card
 export default Card;
