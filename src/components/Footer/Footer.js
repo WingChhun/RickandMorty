a9c05ef6d1198fd26d3277868fd9e863b5bef5de
WingChhun <capt.wing.chhun@gmail.com>
2018-05-27 17:40:23 -0700

    Finished footer markup and stylingh

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,39 @@ class Footer extends Component
     render()
     {
         return (
-            <div>
-                <h1>Footer</h1>
-            </div>
+            <footer className="footer">
+                <ul className="footer__icon">
+                    <li className="footer__container">
+
+                        <a
+                            href="https://github.com/WingChhun"
+                            className=" footer--icon fa  fa-2x fa-github-square"></a>
+                    </li>
+                    <li className="footer__container">
+                        <a
+                            href="https://www.instagram.com/wingchhun/"
+                            className=" footer--icon fa fa-2x fa-instagram"></a>
+                    </li>
+                    <li className="footer__container">
+                        <a
+                            href="https://codepen.io/WingChhun/"
+                            className=" footer--icon fa fa-2x fa-codepen"></a>
+                    </li>
+                    <li className="footer__container">
+                        <a
+                            href="https://www.linkedin.com/in/james-chhun-16b1b5120/"
+                            className=" footer--icon fa fa-2x fa-linkedin-square"></a>
+                    </li>
+                </ul>
+
+                <p className="copyright">
+                    &copy;
+                    <a
+                        className="footer__portfolio"
+                        href="https://wingchhun.github.io/portfolio.github.io//">James Chhun
+                    </a>2018, rickandmortyapi.com</p>
+
+            </footer>
 
         );
     }
