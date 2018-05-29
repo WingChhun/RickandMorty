c80314f882fa65ef033f17409155f9a8fdf35b36
WingChhun <capt.wing.chhun@gmail.com>
2018-05-29 11:57:09 -0700

    Add some .not.toBe testing

--- src/App.test.js
+++ src/App.test.js
@@ -12,23 +12,25 @@ describe("Should display hello", () => {
         expect(testHello()).toBe("Hello testing");
     });
     it("should fail testHello", () => {
-        expect(testHello()).tof(" testing");
+        expect(testHello()).not.toBe(" testing");
     });
     //* Test updateAPI
 
 });
 describe("Test functions from Main Component", () => {
     //* Test nextPage
-    it("should increment nextPage", () => {
+    it("should increment nextPage, and not increment 25 -> 26", () => {
         expect(nextPage(1)).toBe(2);
         expect(nextPage(25)).toBe(25);
+        expect(nextPage(25)).not.toBe(26);
     })
 
     //* Test prevPage
 
-    it("should drecrement prevPage", () => {
+    it("should decrement prevPage and not decrement 1 to 0", () => {
         expect(prevPage(25)).toBe(24);
         expect(prevPage(1)).toBe(1);
+        expect(prevPage(1)).not.toBe(0);
     });
     //* Test updateAPI
     it("Should update info in State", () => {
