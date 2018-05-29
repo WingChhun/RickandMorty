a1a455e8447c9bd328866011117543975ecfeb5b
WingChhun <capt.wing.chhun@gmail.com>
2018-05-28 17:50:07 -0700

    Added pagination markup

--- src/components/Searchbox/Searchbox.js
+++ src/components/Searchbox/Searchbox.js
@@ -15,9 +15,16 @@ class Searchbox extends Component
                     onChange={updateSearch}
                     placeholder="Enter a name to filter for on this page"/> {/*Font*/}
                 <p className="searchbox__under">This app is meant to be displayed on desktop, and was built using the
-                    <a href="rickandmortyapi.com"> Rick and Morty API
+                    <a href="rickandmortyapi.com">
+                        Rick and Morty API
                     </a>
                 </p>
+                <p className="searchbox__under">
+                    Click on 
+                      <span className="u-color-portal-dark">
+                          'prev' or 'next'
+                    </span>
+                    to load more data</p>
 
             </div>
 
